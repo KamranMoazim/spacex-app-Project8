91cc95de84754c4fb05c3b5f1fe3ff86c5e01c7c
CamrunMoazim <kamrannaseer765@gmail.com>
2021-01-23 12:24:25 +0500

    Update AllMissions.tsx

--- src/components/AllMissions/AllMissions.tsx
+++ src/components/AllMissions/AllMissions.tsx
@@ -6,11 +6,14 @@ import {AllMissionsInfoQuery} from '../../generated/graphql'
 
 interface Props {
     data: AllMissionsInfoQuery;
+    error: Error,
+    loading: any
   }
 
 function AllMissions() {
 
     const { data, error, loading }: React.FC<Props> = useAllMissionsInfoQuery();   //  error, loading
+    // const { data, error, loading }: <AllMissionsInfoQuery, Exact<{ [key: string]: never; }>> = useAllMissionsInfoQuery();   //  error, loading
 
     // var data1:AllMissionsInfoQuery = data
 
